d995820c6152bef3cf149da8cf256732bf9eb8bc
Simon Karman <mail@simonkarman.nl>
2023-11-23 22:43:48 +0100

    On link send all previously dispatched system messages first as a sys-batch message

--- client/src/components/layer.tsx
+++ client/src/components/layer.tsx
@@ -1,4 +1,5 @@
 import { KrmxProvider, useKrmx } from '@krmx/client';
+import { createHash } from 'crypto';
 import { createContext, MutableRefObject, PropsWithChildren, useContext, useEffect, useRef, useState } from 'react';
 import { system, System, Action, SystemState } from 'system';
 
@@ -20,15 +21,31 @@ export const useSystem = () => {
 export function KrmxLayer(props: PropsWithChildren) {
   const [serverUrl] = useState('ws://localhost:8082');
   const systemRef = useRef(system);
+  const handlePayload = (messagePayload: unknown) => {
+    const { dispatcher, type, payload, hash } = messagePayload as unknown as { dispatcher: string, type: string, payload: unknown, hash?: string };
+    if (systemRef.current.dispatch(dispatcher, { type, payload }) !== true) {
+      console.error('[ERROR] system dispatch failed', { dispatcher, type, payload });
+    }
+    if (hash) {
+      // @ts-expect-error sourceState is private
+      const state = systemRef.current.sourceState;
+      const computedHash = createHash('sha256').update(JSON.stringify(state)).digest('hex');
+      if (hash !== computedHash) {
+        console.error('[ERROR] system corrupted due to mismatching hashes', hash, computedHash);
+      }
+    }
+  };
   return (
     <KrmxProvider
       serverUrl={serverUrl}
       onMessage={(message) => {
-        if (message?.type === 'sys') {
-          const { dispatcher, type, payload } = message['payload'] as unknown as { dispatcher: string, type: string, payload: unknown };
-          if (systemRef.current.dispatch(dispatcher, { type, payload }) !== true) {
-            console.error('[ERROR] system dispatch failed', { dispatcher, type, payload });
-          }
+        if (message?.type === 'sys-reset') {
+          systemRef.current.reset();
+        } else if (message?.type === 'sys') {
+          handlePayload(message['payload']);
+        } else if (message?.type === 'sys-batch') {
+          console.info('batch', message);
+          message['payload'].forEach(messagePayload => handlePayload(messagePayload));
         }
       }}
     >
@@ -43,18 +60,13 @@ export function KrmxSystem(props: PropsWithChildren<{
   systemRef: MutableRefObject<System<{counter: number}>>,
 }>) {
   const { systemRef } = props;
-  const { isLinked, username, send } = useKrmx();
+  const { username, send } = useKrmx();
   const [state, setState] = useState(system.initialState);
   const [optimisticState, setOptimisticState] = useState(system.initialState);
   useEffect(() => {
     systemRef.current.onChange((state) => setState(state));
     systemRef.current.onOptimisticChange((state) => setOptimisticState(state));
   }, []);
-  useEffect(() => {
-    if (isLinked) {
-      systemRef.current.reset();
-    }
-  }, [isLinked]);
   const dispatcher = (action: Action): boolean => {
     if (systemRef.current.dispatch(username, action, true) === true) {
       send(action);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,68 @@
-import { createServer, Props } from '@krmx/server';
+import { createServer, Message, Props } from '@krmx/server';
+import { createHash } from 'crypto';
 import { system } from 'system';
+
+const BATCH_SIZE = 100;
 
 const props: Props = { /* configure here */ };
 const server = createServer(props);
 
-system.onChange(console.info);
+type SysMessagePayload = {
+  dispatcher: string,
+  hash?: string,
+} & Message;
 
-// TODO: keep track of all dispatched messages including username and timestamp
-// TODO: on link send all previously dispatched messages first as a sys-batch message (per X message to avoid frame size limits of websockets)
-// TODO: on shutdown, save all dispatched messages to a history file for re-watch functionality
+interface SysMessage {
+  type: 'sys',
+  payload: SysMessagePayload,
+}
+interface BatchSysMessage {
+  type: 'sys-batch',
+  payload: SysMessagePayload[],
+}
 
-server.on('message', (username, message) => {
-  console.info(username, message);
-  if (system.dispatch(username, message) === true) {
-    const broadcastMessage = {
-      type: 'sys',
-      payload: {
-        dispatcher: username,
-        ...message,
-        // TODO: every X updates include a hash of the state to the client, to verify state equality
-        hash: undefined,
-      },
+// Keep track of all dispatched messages (including username and timestamp)
+const history: SysMessagePayload[] = [];
+
+// On link send all previously dispatched messages first as a sys-batch message (per X message to avoid frame size limits of websockets)
+server.on('link', (username) => {
+  server.send(username, { type: 'sys-reset' });
+  for (let i = 0; i < history.length; i += BATCH_SIZE) {
+    const batchSysMessage: BatchSysMessage = {
+      type: 'sys-batch',
+      payload: history.slice(i, i + BATCH_SIZE),
     };
-    console.info(broadcastMessage);
-    server.broadcast(broadcastMessage);
-
+    server.send(username, batchSysMessage);
   }
 });
+
+// Keep track of a history of hashes of the state
+const hashHistory: string[] = [];
+system.onChange((state) => {
+  if (history.length % BATCH_SIZE === 0) {
+    hashHistory.push(createHash('sha256').update(JSON.stringify(state)).digest('hex'));
+  }
+});
+
+// When we receive a message from a client, verify it against the system and broadcast on success
+server.on('message', (username, message) => {
+  if (system.dispatch(username, message) === true) {
+    const payload: SysMessagePayload = {
+      dispatcher: username,
+      ...message,
+      // Every X updates include a hash of the state to the client, to verify state equality
+      hash: history.length % BATCH_SIZE === 0 ? hashHistory[hashHistory.length - 1] : undefined,
+    };
+    history.push(payload);
+
+    const sysMessage: SysMessage = {
+      type: 'sys',
+      payload,
+    };
+    server.broadcast(sysMessage);
+  }
+});
+
+// TODO: on shutdown, save all dispatched messages to a history file for re-watch functionality
 
 server.listen(8082);
